e867bb59e9ff8fa0fe01e0e2e5701ea23a8d86f7
OlamilekanAdewuyi <olamilekanadewuyi90@gmail.com>
2025-10-15 15:16:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// Addition
+const addition = () => {
+    const convertedfirstNumber = Number(firstNumber.value);
+    // console.log(convertedfirstNumber);
+    const convertedsecondNumber = Number(secondNumber.value)
+    // console.log(convertedsecondNumber);
+    const answer = (firstNumber.value + secondNumber.value).toFixed(3)
+    // console.log(answer);
+    showResult.innerHTML = answer
+}
+
+// Multiplication
+const multiply = () => {
+    // console.log(firstNumber.value);
+    // console.log(secondNumber.value);
+    const answer = (firstNumber.value * secondNumber.value).toFixed(3)
+    // console.log(answer);
+    showResult.innerHTML = answer
+}
+// Subtraction
+const subtraction = () => {
+    // console.log(firstNumber.value);
+    // console.log(secondNumber.value);
+   const answer = (firstNumber.value - secondNumber.value).toFixed(3)
+    // console.log(answer);
+    showResult.innerHTML = answer
+}
+
+// Division
+const division = () => {
+    // console.log(firstNumber.value);
+    // console.log(secondNumber.value);
+    const answer = (firstNumber.value / secondNumber.value).toFixed(3)
+    // console.log(answer);
+    showResult.innerHTML = answer
+}
